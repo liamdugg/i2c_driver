b29cfb8a018d021866494ec3c82372c01ec5b18a
liam <liamdug@mail.com>
2024-01-09 00:43:47 +0000

    changes to i2c.c

--- driver/inc/i2c.h
+++ driver/inc/i2c.h
@@ -20,119 +20,120 @@
 #include <linux/of_clk.h>
 #include <linux/clk.h>
 
-#define DRIVER_NAME "i2c_lliano"
-
-int i2c_init(struct platform_device *pdev);
 void i2c_deinit(void);
-int i2c_write(char slave_address, char* data, char size);
-int i2c_read(char slave_address, char* read_buff, char size);
-int i2c_read_reg(char slave_address, char reg_address, char* read_buff);
+int  i2c_init(struct platform_device *pdev);
+int  i2c_write(char slave_address, char* data, char size);
+int  i2c_read(char slave_address, char* read_buff, char size);
 
-#define TIMEOUT_READ_WRITE 100  // msec
+typedef struct {
+    
+    uint8_t * tx_buf;
+	uint8_t * rx_buf;  
+    
+    uint8_t tx_pos;
+	uint8_t rx_pos;   
 
-#define DT_PROPERTY_PINMUX_PHANDLE  "pinmux"
-#define DT_PROPERTY_PINS            "pins"
-#define DT_PROPERTY_CLK_PHANDLE     "clocks"
-#define DT_PROPERTY_CLK_FREQ        "clock-frequency"
-#define DT_PROPERTY_INT_CLK_FREQ    "int-clock-frequency"
-#define DT_PROPERTY_BIT_RATE        "bit-rate"
+    uint8_t rx_len;
+    uint8_t tx_len;
 
-// Clock Module Peripheral (CM_PER) (page 179) Length = 1K-> 0x400
-#define CM_PER                  0x44E00000
-#define CM_PER_LEN              0x00000400 // 1K
+	uint32_t virq;
 
-// I2C2 clocks manager (page 1270)
-#define IDCM_PER_I2C2_CLKCTRL      0x00000044
-#define CM_PER_I2C2_CLKCTRL_MASK   0x00030003
-#define CM_PER_I2C2_CLKCTRL_ENABLE 0x00000002
+} i2c_handler_t;
 
-// Config de Control Module (page180)
-#define CTRL_MODULE_BASE        0x44E10000
-#define CTRL_MODULE_LEN         0x00002000 // 8K la datasheet dice 128k pero el fin es en @x44E11FFF 0x00000978 //pin 20 = sda (page1461)
-#define CTRL_MODULE_UART1_CTSN  0x00000978 //pin 20 = sda (page1461)  
-#define CTRL_MODULE_UART1_RTSN  0x0000097C //pin 19 = scl (page1461) 
-#define CTRL_MODULE_UART1_MASK  0x0000007F
+// clock module peripheral register
 
-#define CTRL_MOD_SCL			0x97C
-#define CTRL_MOD_SDA			0x978
+#define CM_PER_BASE			0x44E00000
+#define CM_PER_LEN			0x400
 
-#define CTRL_MOD_SCL_MODE		0x33
-#define CTRL_MOD_SDA_MODE		0x33
+#define CM_PER_CLKCTRL		0x44
+#define CM_PER_CLKCTRL_ENA	0x2
+#define CM_PER_CLKCTRL_MASK	0x00030003
 
-#define CTRL_MOD_SCL_MSK		0xFFFFFFC0
-#define CTRL_MOD_SDA_MSK		0xFFFFFFC0
+// control module
 
-// I2C2 Module (page183)
-#define I2C2                    0x4819C000
-#define I2C2_LEN                0x00001000
+#define CTRL_MOD_BASE		0x44E10000
+#define CTRL_MOD_LEN		0x2000 
 
-// Registers' offset address
-#define I2C_REG_REVNB_LO        0x00
-#define I2C_REG_REVNB_HI        0x04
-#define I2C_REG_SYSC            0x10
-#define I2C_REG_IRQSTATUS_RAW   0x28
-#define I2C_REG_IRQSTATUS       0x28
-#define I2C_REG_IRQENABLE_SET   0x2C
-#define I2C_REG_IRQENABLE_CLR   0x30
-#define I2C_REG_WE              0x34
-#define I2C_REG_SYSS            0x90
-#define I2C_REG_BUF             0x94
-#define I2C_REG_CNT             0x98
-#define I2C_REG_DATA            0x9C
-#define I2C_REG_CON             0xA4
-#define I2C_REG_OA              0xA8
-#define I2C_REG_SA              0xAC
-#define I2C_REG_PSC             0xB0
-#define I2C_REG_SCLL            0xB4
-#define I2C_REG_SCLH            0xB8
+#define CTRL_MOD_SCL		0x97C
+#define CTRL_MOD_SDA		0x978
+
+#define CTRL_MOD_SCL_MODE	0x33
+#define CTRL_MOD_SDA_MODE	0x33
+
+#define CTRL_MOD_SCL_MSK	0xFFFFFFC0
+#define CTRL_MOD_SDA_MSK	0xFFFFFFC0
+
+// I2C2
+
+#define I2C_BASE			0x4819C000
+#define I2C_LEN				0x1000
+
+#define I2C_REVNB_LO		0x00
+#define I2C_REVNB_HI		0x04
+#define I2C_SYSC            0x10
+#define I2C_IRQSTAT_RAW		0x24
+#define I2C_IRQSTAT			0x28
+#define I2C_IRQENA_SET		0x2C
+#define I2C_IRQENA_CLR 		0x30
+#define I2C_WE				0x34
+#define I2C_SYSS			0x90
+#define I2C_BUF				0x94
+#define I2C_CNT				0x98
+#define I2C_DATA			0x9C
+#define I2C_CON				0xA4
+#define I2C_OA				0xA8
+#define I2C_SA				0xAC
+#define I2C_PSC				0xB0
+#define I2C_SCLL			0xB4
+#define I2C_SCLH			0xB8
 
 // CON
-#define I2C_BIT_ENABLE          (1 << 15)
-#define I2C_BIT_MASTER_MODE     (1 << 10)
-#define I2C_BIT_TX              (1 << 9)
-#define I2C_BIT_RX              (1 << 9)
-#define I2C_BIT_STOP            (1 << 1)
-#define I2C_BIT_START           (1 << 0)
+#define I2C_CON_START		(1 << 0)
+#define I2C_CON_STOP		(1 << 1)
+#define I2C_CON_TX			(1 << 9)
+#define I2C_CON_RX			(1 << 9)
+#define I2C_CON_MASTER		(1 << 10)
+#define I2C_CON_ENABLE		(1 << 15)
 
 // SYSC
-#define I2C_BIT_AUTOIDLE        (1 << 0)
-#define I2C_BIT_RESET           (1 << 1)
-#define I2C_BIT_WAKEUP          (1 << 2)    // Enable own wakeup
-#define I2C_BIT_NOIDLE          (1 << 3)    // No idle
-#define I2C_BIT_CLKACTIVITY     (3 << 8)    // Both clocks active
+#define I2C_SYSC_AUTOIDLE	(1 << 0)
+#define I2C_SYSC_RESET		(1 << 1)
+#define I2C_SYSC_WAKEUP		(1 << 2)
+#define I2C_SYSC_NOIDLE		(1 << 3)
+#define I2C_SYSC_CLKACT		(3 << 8)
 
 // IRQSTATUS
-#define I2C_IRQ_BB              (1 << 12)
-#define I2C_IRQ_XRDY            (1 << 4)
-#define I2C_IRQ_RRDY            (1 << 3)
-#define I2C_IRQ_ARDY            (1 << 2)
-#define I2C_IRQ_NACK            (1 << 1)
-#define I2C_IRQ_AL              (1 << 0)
+#define I2C_IRQ_AL			(1 << 0)
+#define I2C_IRQ_NACK		(1 << 1)
+#define I2C_IRQ_ARDY		(1 << 2)
+#define I2C_IRQ_RRDY		(1 << 3)
+#define I2C_IRQ_XRDY		(1 << 4)
+#define I2C_IRQ_BB			(1 << 12)
 
-#define I2C_IRQSTATUS_CLR_ALL   0x00006FFF
+#define I2C_IRQSTAT_CLR_ALL	0x00006FFF
 
-#define I2C_IRQENABLE_CLR_MASK  0x00006FFF
-#define I2C_IRQENABLE_CLR_ACK   0x00000004
-#define I2C_IRQENABLE_CLR_RX    0x00000008
-#define I2C_IRQENABLE_CLR_TX    0x00000010
+#define I2C_IRQENA_CLR_MASK	0x00006FFF
+#define I2C_IRQENA_CLR_ACK	0x00000004
+#define I2C_IRQENA_CLR_RX	0x00000008
+#define I2C_IRQENA_CLR_TX	0x00000010
 
 // BUF
-#define I2C_BIT_RXFIFO_CLR      (1 << 14)
-#define I2C_BIT_TXFIFO_CLR      (1 << 6)
+//#define I2C_BIT_RXFIFO_CLR      (1 << 14)
+//#define I2C_BIT_TXFIFO_CLR      (1 << 6)
 
 // PSC
-#define I2C_PSC_MASK            0x000000FF
-#define I2C_PSC_12MHZ           0x00000003 // divided by 3, 48MHz/4 = 12MHz (page4589)
-#define I2C_PSC_24MHZ           0x00000001 // divided by 1, 48MHz/1 = 24MHz (page4589)
+#define I2C_PSC_MASK		0x000000FF
+#define I2C_PSC_12MHZ		0x00000003 // div por 3, 48MHz/4 = 12MHz 
+#define I2C_PSC_24MHZ		0x00000001 // div por 1, 48MHz/1 = 24MHz
 
 // SCLL
-#define I2C_SCLL_MASK           0x000000FF
-#define I2C_SCLL_400K           0x00000017 // tLOW = 1,25 US = (23+7)*(2/48MHz)
-#define I2C_SCLL_100K           0x000000E9 // tLOW = 5 US = (233+7)+(1/48MHz)
+#define I2C_SCLL_MASK		0x000000FF
+#define I2C_SCLL_400K		0x00000017 // tLOW = 1,25 uS = (23+7)*(2/48MHz)
+#define I2C_SCLL_100K		0x000000E9 // tLOW = 5,00 uS = (233+7)+(1/48MHz)		
 
 // SCLH
-#define I2C_SCLH_MASK           0x000000FF
-#define I2C_SCLH_400K           0x00000019 // THIGH = 1,25 uS = (25+5)*(1/48MHz)
-#define I2C_SCLH_100K           0x000000EB // THIGH = 5 us = (235+5)*(1/48MHz)
+#define I2C_SCLH_MASK		0x000000FF
+#define I2C_SCLH_400K		0x00000019 // THIGH = 1,25 uS = (25+5)*(1/48MHz)
+#define I2C_SCLH_100K		0x000000EB // THIGH = 5,00 uS = (235+5)*(1/48MHz)
 
 #endif // _I2C_H
--- driver/src/i2c.c
+++ driver/src/i2c.c
@@ -1,6 +1,6 @@
 #include "../inc/i2c.h"
 
-/* --------------- VARIABLES ---------------*/
+/* --------------- VARIABLES --------------- */
 
 static void __iomem *i2c_ptr = NULL;
 static void __iomem *clk_ptr = NULL;
@@ -9,99 +9,51 @@ static void __iomem *control_module_ptr = NULL;
 DEFINE_MUTEX(lock_bus);
 DECLARE_WAIT_QUEUE_HEAD(waitq);
 
-// I2C Data structure
-static struct i2c_buffers{
-    uint8_t * buff_rx;  // Pointer to user data in kernel space
-    uint8_t pos_rx;     // Data to be received. This will be updated by the ISR.
-    uint8_t buff_rx_len;
+static i2c_handler_t hm_i2c;
 
-    uint8_t * buff_tx;
-    uint8_t pos_tx;
-    uint8_t buff_tx_len;
-} data_i2c;
+int waitq_cond;
 
-int waitq_cond;  // Condition to handle the state of the processes.
-static int g_irq;       // IRQ number, saved for deinitialization
+/* --------------- PROTOTIPOS --------------- */
 
-/* --------------- PROTOTIPOS ---------------*/
 static int  i2c_is_busy(void);
+static int  i2c_set_virq(struct platform_device *pdev);
 static int  i2c_is_locked(void);
 static void i2c_set_slave(uint8_t slv_addr);
 static void i2c_power_clock(void);
 static void i2c_clear_bufs(void);
 
+/* --------------- FUNCIONES --------------- */
 
-static void i2c_set_slave(uint8_t addr){
-    iowrite32(addr, i2c_ptr + I2C_REG_SA);
-}
-
-static void i2c_power_clock(void){
-
-    uint32_t aux;
-
-    //aux = (uint32_t)clk_ptr + (unsigned int)(IDCM_PER_I2C2_CLKCTRL); 
-    aux = ioread32(clk_ptr + IDCM_PER_I2C2_CLKCTRL); 
-    aux |= 0x02;
-    iowrite32(aux, clk_ptr + IDCM_PER_I2C2_CLKCTRL);
-    	 while(ioread32(clk_ptr + IDCM_PER_I2C2_CLKCTRL) != CM_PER_I2C2_CLKCTRL_ENABLE);
-}
-
-static int i2c_is_locked(void) {
+static irqreturn_t i2c_handler(int irq_number, void *dev_id) {
     
-	uint8_t i = 0;
-
-    while(mutex_is_locked(&lock_bus)) {
-        msleep(1);
-        if (i++ == 100) {
-            pr_warn("%s: TIMEOUT ERROR: I2C bus is locked.\n", DRIVER_NAME);
-            return -1;
-        }
-    }
-
-    return 0;
-}
-
-static void i2c_clear_bufs(void){
-
-    memset(data_i2c.buff_rx, 0, 4096);
-    data_i2c.pos_rx = 0;
-    data_i2c.buff_rx_len = 0;
-
-    memset(data_i2c.buff_tx, 0, 4096);
-    data_i2c.pos_tx = 0;
-    data_i2c.buff_tx_len = 0;
-}
-
-static irqreturn_t i2c_isr(int irq_number, void *dev_id) {
-    
-	int irq = ioread32(i2c_ptr + I2C_REG_IRQSTATUS);
+	int irq = ioread32(i2c_ptr + I2C_IRQSTAT);
 
 	// TX 
     if(irq & I2C_IRQ_XRDY){ 
         
-        iowrite32(data_i2c.buff_tx[data_i2c.pos_tx++], i2c_ptr + I2C_REG_DATA);
+        iowrite32(hm_i2c.tx_buf[hm_i2c.tx_pos++], i2c_ptr + I2C_DATA);
     
-        if(data_i2c.buff_tx_len == data_i2c.pos_tx){
-            iowrite32(I2C_IRQENABLE_CLR_TX, i2c_ptr + I2C_REG_IRQENABLE_CLR);
+        if(hm_i2c.tx_len == hm_i2c.tx_pos){
+            iowrite32(I2C_IRQENA_CLR_TX, i2c_ptr + I2C_IRQENA_CLR);
             waitq_cond = 1;
             wake_up_interruptible(&waitq);
         }
 
-        iowrite32(I2C_IRQSTATUS_CLR_ALL, i2c_ptr + I2C_REG_IRQSTATUS);
+        iowrite32(I2C_IRQSTAT_CLR_ALL, i2c_ptr + I2C_IRQSTAT);
     }
 	
 	// RX
     if(irq & I2C_IRQ_RRDY){
         
-        data_i2c.buff_rx[data_i2c.pos_rx++] = ioread32(i2c_ptr + I2C_REG_DATA);
+        hm_i2c.rx_buf[hm_i2c.rx_pos++] = ioread32(i2c_ptr + I2C_DATA);
 
-        if(data_i2c.buff_rx_len == data_i2c.pos_rx){ 
-            iowrite32(I2C_IRQENABLE_CLR_RX, i2c_ptr + I2C_REG_IRQENABLE_CLR);
+        if(hm_i2c.rx_len == hm_i2c.rx_pos){ 
+            iowrite32(I2C_IRQENA_CLR_RX, i2c_ptr + I2C_IRQENA_CLR);
             waitq_cond = 1;
             wake_up_interruptible(&waitq);
         }
 
-        iowrite32(I2C_IRQSTATUS_CLR_ALL, i2c_ptr + I2C_REG_IRQSTATUS);
+        iowrite32(I2C_IRQSTAT_CLR_ALL, i2c_ptr + I2C_IRQSTAT);
     }  
 
     return IRQ_HANDLED;
@@ -109,40 +61,36 @@ static irqreturn_t i2c_isr(int irq_number, void *dev_id) {
 
 int i2c_init(struct platform_device *pdev) {
     
-	struct device *i2c_dev = &pdev->dev;
-    int retval = -1;
 	uint32_t reg;
+	struct device *i2c_dev = &pdev->dev;
 
-    // Check that parent device exists (should be target-module@9c000)
     if (i2c_dev->parent == NULL) {
-        pr_err("I2C device doesn't have a parent.\n");
-        goto pdev_error;
+        pr_err("BMP --> Error, i2c_dev no tiene un dev padre.\n");
+        return -1;
     }
 
-    // Mapeo registros
+	/* ----- mapeo de direcciones base ----- */
 
-    if((clk_ptr = ioremap(CM_PER, CM_PER_LEN)) == NULL){
-        pr_alert("%s: Could not assign memory for clk_ptr (CM_PER).\n", DRIVER_NAME);
-        goto pdev_error;
+    if((clk_ptr = ioremap(CM_PER_BASE, CM_PER_LEN)) == NULL){
+        pr_err("BMP --> Error al mapear clk_ptr.\n");
+        return -1;
     }
 
-    pr_info("%s: clk_ptr: 0x%X\n", DRIVER_NAME, (unsigned int)clk_ptr);
-
-    if((control_module_ptr = ioremap(CTRL_MODULE_BASE, CTRL_MODULE_LEN)) == NULL){
-        pr_alert("%s: Could not assign memory for control_module_ptr (CTRL_MODULE_BASE).\n", DRIVER_NAME);
-        goto clk_ptr_error;
+    if((control_module_ptr = ioremap(CTRL_MOD_BASE, CTRL_MOD_LEN)) == NULL){
+        pr_err("BMP --> Error al mapear control_module_ptr.\n");
+		iounmap(clk_ptr);
+		return -1;
     }
 
-    pr_info("%s: control_module_ptr: 0x%X\n", DRIVER_NAME, (unsigned int)control_module_ptr);
-
-    if((i2c_ptr = ioremap(I2C2, I2C2_LEN)) == NULL){
-        pr_alert("%s: Could not assign memory for i2c_ptr (I2C2).\n", DRIVER_NAME);
-        goto control_module_ptr_error;
+    if((i2c_ptr = ioremap(I2C_BASE, I2C_LEN)) == NULL){
+        pr_err("BMP --> Error al mapear i2c_ptr.\n");
+        iounmap(clk_ptr);
+		iounmap(control_module_ptr);
+		return -1;
     }
 
-    pr_info("%s: i2c_ptr: 0x%X\n", DRIVER_NAME, (unsigned int)i2c_ptr);
+	/* ----- configuro pines SDA y SCL ----- */
 
-    // Pinmux configuration
     reg = ioread32(control_module_ptr + CTRL_MOD_SCL);
 	reg &= CTRL_MOD_SCL_MSK;
 	reg |= CTRL_MOD_SCL_MODE;
@@ -153,58 +101,48 @@ int i2c_init(struct platform_device *pdev) {
 	reg |= CTRL_MOD_SDA_MODE;
 	iowrite32(reg, control_module_ptr + CTRL_MOD_SDA);
 
-    // Turn ON I2C Clock
+	/* ----- configuro clock ----- */
+
     i2c_power_clock();
 
-    // Disable I2C while configuring..
-    iowrite32(0x0, i2c_ptr + I2C_REG_CON); 
+    // deshabilito i2c
+    iowrite32(0x0, i2c_ptr + I2C_CON); 
 
-    // Clock Configuration
-    iowrite32(I2C_PSC_24MHZ, i2c_ptr + I2C_REG_PSC);
-    iowrite32(I2C_SCLL_400K, i2c_ptr + I2C_REG_SCLL); 
-    iowrite32(I2C_SCLH_400K, i2c_ptr + I2C_REG_SCLH); 
+    iowrite32(I2C_PSC_24MHZ, i2c_ptr + I2C_PSC);
+    iowrite32(I2C_SCLL_400K, i2c_ptr + I2C_SCLL); 
+    iowrite32(I2C_SCLH_400K, i2c_ptr + I2C_SCLH); 
 
-    // Force Idle
-    iowrite32(0x00, i2c_ptr + I2C_REG_SYSC);   
+	/* ----- configuro i2c_con ----- */
     
-    // Enable I2C device
-    iowrite32(I2C_BIT_ENABLE | I2C_BIT_MASTER_MODE | I2C_BIT_TX, // 0x8600
-        i2c_ptr + I2C_REG_CON);
+	iowrite32(0x00, i2c_ptr + I2C_SYSC);   
+    iowrite32(I2C_CON_ENABLE | I2C_CON_MASTER | I2C_CON_TX, i2c_ptr + I2C_CON);
     
     // Virtual IRQ request
-    if ((g_irq = platform_get_irq(pdev, 0)) < 0) {
-        pr_err("%s: Couldn't get I2C IRQ number.\n", DRIVER_NAME);
-        goto i2c_ptr_error;
+    if(i2c_set_virq(pdev) != 0){
+		return -1;
+	}
+
+    if ((hm_i2c.rx_buf = (char *) __get_free_page (GFP_KERNEL)) < 0){
+        pr_alert("BMP --> Error, no se pudo obtener memoria para rx_buf.\n"); 
+		iounmap(i2c_ptr);
+		iounmap(clk_ptr);
+		iounmap(control_module_ptr);
+		free_irq(hm_i2c.virq, NULL);
+		return -1;
     }
 
-    if ((request_irq(g_irq, (irq_handler_t) i2c_isr, IRQF_TRIGGER_RISING, "lliano,i2c", NULL) < 0)){//pdev->name, NULL)) < 0) {
-        pr_err("%s: Couldn't request I2C IRQ.\n", DRIVER_NAME);
-        goto i2c_ptr_error;
+    if ((hm_i2c.tx_buf = (char *) __get_free_page (GFP_KERNEL)) < 0){
+        pr_alert("BMP --> Error, no se pudo obtener memoria para tx_buf.\n");
+        free_page((unsigned long)hm_i2c.rx_buf);
+		iounmap(i2c_ptr);
+		iounmap(clk_ptr);
+		iounmap(control_module_ptr);
+		free_irq(hm_i2c.virq, NULL);
+		return -1;
     }
 
-    // Internal struct init.
-
-    // We ask the kernel for memory for the buffers (4kB)
-    if ((data_i2c.buff_rx = (char *) __get_free_page (GFP_KERNEL)) < 0){
-        pr_alert("%s: Error while asking por a free page.\n", DRIVER_NAME);
-        goto virq_error;
-    }
-
-    if ((data_i2c.buff_tx = (char *) __get_free_page (GFP_KERNEL)) < 0){
-        pr_alert("%s: Error while asking por a free page.\n", DRIVER_NAME);
-        goto virq_error;
-    }
-
-    pr_info("I2C successfully configured.\n");
+    pr_info("BMP --> I2C Configurado exitosamente.\n");
     return 0;
-
-    // Error Handling
-    virq_error: free_irq(g_irq, NULL);
-    i2c_ptr_error: iounmap(i2c_ptr);
-    control_module_ptr_error: iounmap(control_module_ptr);
-    clk_ptr_error: iounmap(clk_ptr);
-    pdev_error: retval = -1; i2c_ptr = NULL; clk_ptr = NULL; control_module_ptr = NULL;
-    return retval;
 }
 
 void i2c_deinit(void) {
@@ -218,76 +156,74 @@ void i2c_deinit(void) {
 	if(i2c_ptr != NULL)
         iounmap(i2c_ptr);
 
-    free_irq(g_irq, NULL);
-    free_page((unsigned long)data_i2c.buff_rx); 
-    free_page((unsigned long)data_i2c.buff_tx);
+    free_irq(hm_i2c.virq, NULL);
+    free_page((unsigned long)hm_i2c.rx_buf); 
+    free_page((unsigned long)hm_i2c.tx_buf);
 }
 
 int i2c_write(char slave_address, char* data, char size){
 
     int retval = -1;
-    int auxReg;
+    uint32_t reg;
 
     if (size == 0){
-        pr_warn("%s: Write Error: Size should be greater than 0.\n", DRIVER_NAME);
-        return retval;
+        pr_err("BMP --> Error, el tamaño no puede ser cero.\n");
+        return -1;
     }
 
-    // Wait until no other process is using it
+	// chequeo que no este lockeado el mutex
     if(i2c_is_locked() != 0) {
-        return retval;
+        return -1;
     }
 
-    // Get control of the bus
     mutex_lock(&lock_bus);
 
-    // Makes sure CLK is running
+	// prendo el clock, se suele trabar si no lo hago
     i2c_power_clock();
     
-    // Set slave address
     i2c_set_slave(slave_address);
     
-    // Load the data structures and registers.
     i2c_clear_bufs();
-    memcpy(data_i2c.buff_tx, data, size);
-    data_i2c.buff_tx_len = size;
+    memcpy(hm_i2c.tx_buf, data, size);
+    hm_i2c.tx_len = size;
 
     // Load I2C CNT registers
-    iowrite32(data_i2c.buff_tx_len, i2c_ptr + I2C_REG_CNT);
+    iowrite32(hm_i2c.tx_len, i2c_ptr + I2C_CNT);
 
-    // Sets I2C CONFIG register w/ Master TX (=0x8600)
-    iowrite32(I2C_BIT_ENABLE | I2C_BIT_MASTER_MODE | I2C_BIT_TX, i2c_ptr + I2C_REG_CON);
+    // habilito, modo master y tx
+    iowrite32(I2C_CON_ENABLE | I2C_CON_MASTER | I2C_CON_TX, i2c_ptr + I2C_CON);
 
-    // Clear IRQ Flags
-    iowrite32(I2C_IRQSTATUS_CLR_ALL, i2c_ptr + I2C_REG_IRQENABLE_CLR); 
-    iowrite32(I2C_IRQSTATUS_CLR_ALL, i2c_ptr + I2C_REG_IRQSTATUS);
+    // limpio flags de IRQ y las deshabilito
+    iowrite32(I2C_IRQSTAT_CLR_ALL, i2c_ptr + I2C_IRQENA_CLR); 
+    iowrite32(I2C_IRQSTAT_CLR_ALL, i2c_ptr + I2C_IRQSTAT);
 
-    // Enables ACK interrupt
-    iowrite32(I2C_IRQ_XRDY, i2c_ptr + I2C_REG_IRQENABLE_SET);
+    // habilito IRQ de TX
+    iowrite32(I2C_IRQ_XRDY, i2c_ptr + I2C_IRQENA_SET);
 
-    // Check irq status (occupied or free)
-    while (ioread32(i2c_ptr + I2C_REG_IRQSTATUS_RAW) & I2C_IRQ_BB) {msleep(1);}
+    // chequeo si el bus esta ocupado
+    while (ioread32(i2c_ptr + I2C_IRQSTAT_RAW) & I2C_IRQ_BB) {
+		msleep(1);
+	}
 
-    // Sends START
-    waitq_cond = 0;     // We need to ensure the condition before sending the start
+	waitq_cond = 0;
 
-    auxReg = ioread32(i2c_ptr + I2C_REG_CON); 
-    auxReg |= I2C_BIT_START;
-    iowrite32(auxReg, i2c_ptr + I2C_REG_CON);
+	// start condition
+    reg = ioread32(i2c_ptr + I2C_CON); 
+    reg |= I2C_CON_START;
+    iowrite32(reg, i2c_ptr + I2C_CON);
 
-    // Sends process to sleep
     wait_event_interruptible (waitq, waitq_cond != 0);
 
-    // We clear the start bit and sets the stop
-    auxReg = ioread32(i2c_ptr + I2C_REG_CON);
-    auxReg &= 0xFFFFFFFE;
-    auxReg |= I2C_BIT_STOP;
-    iowrite32(auxReg, i2c_ptr + I2C_REG_CON);
+    // stop condition
+    reg = ioread32(i2c_ptr + I2C_CON);
+    reg &= 0xFFFFFFFE;
+    reg |= I2C_CON_STOP;
+    iowrite32(reg, i2c_ptr + I2C_CON);
 
-    // Waits for the core to send the stop and frees the mutex.
-    udelay(100); // 100 us
+    udelay(100);
     mutex_unlock(&lock_bus);
-    if (waitq_cond > 0)
+    
+	if (waitq_cond > 0)
         retval = 0;
 
     return retval;
@@ -296,139 +232,131 @@ int i2c_write(char slave_address, char* data, char size){
 int i2c_read(char slave_address, char* read_buff, char size){
 
     int retval = -1;
-    int auxReg;
+    uint32_t reg;
 
     if (size == 0){
-        pr_warn("%s: Write Error: Size should be greater than 0.\n", DRIVER_NAME);
-        return retval;
+        pr_err("BMP --> Error, el tamaño no puede ser cero.\n");
+        return -1;
     }
 
-    // Wait until no other process is using it
     if(i2c_is_locked() != 0) {
-        return retval;
+        return -1;
     }
 
-    // Get control of the bus
     mutex_lock(&lock_bus);
 
-    // Makes sure CLK is running
     i2c_power_clock();
 
     // Clear IRQ Flags
-    iowrite32(I2C_IRQSTATUS_CLR_ALL, i2c_ptr + I2C_REG_IRQENABLE_CLR); 
-    iowrite32(I2C_IRQSTATUS_CLR_ALL, i2c_ptr + I2C_REG_IRQSTATUS);
+    iowrite32(I2C_IRQSTAT_CLR_ALL, i2c_ptr + I2C_IRQENA_CLR); 
+    iowrite32(I2C_IRQSTAT_CLR_ALL, i2c_ptr + I2C_IRQSTAT);
     
+    // habilito IRQ de RX
+    iowrite32(I2C_IRQ_RRDY, i2c_ptr + I2C_IRQENA_SET);
+
     // Set slave address
     i2c_set_slave(slave_address);
     
     // Load the data structures and registers.
     i2c_clear_bufs();
-    data_i2c.buff_rx_len = size;
+    hm_i2c.rx_len = size;
 
     // Load I2C DATA & CNT registers
-    iowrite32(data_i2c.buff_rx_len, i2c_ptr + I2C_REG_CNT);
+    iowrite32(hm_i2c.rx_len, i2c_ptr + I2C_CNT);
 
-    // Sets I2C CONFIG register w/ Master RX (=0x8400)
-    iowrite32(I2C_BIT_ENABLE | I2C_BIT_MASTER_MODE, i2c_ptr + I2C_REG_CON); // (RX is enable with 0 at I2C_BIT_TX)
+    // habilito, modo master y rx
+    iowrite32(I2C_CON_ENABLE | I2C_CON_MASTER, i2c_ptr + I2C_CON);
 
-    // Enables ACK interrupt
-    iowrite32(I2C_IRQ_RRDY, i2c_ptr + I2C_REG_IRQENABLE_SET);
+    // chequeo si el bus esta libres
+    while (ioread32(i2c_ptr + I2C_IRQSTAT_RAW) & I2C_IRQ_BB) {msleep(1);}
 
-    // Check irq status (occupied or free)
-    while (ioread32(i2c_ptr + I2C_REG_IRQSTATUS_RAW) & I2C_IRQ_BB) {msleep(1);}
-
-    // Sends START
     waitq_cond = 0;
 
-    auxReg = ioread32(i2c_ptr + I2C_REG_CON); 
-    auxReg |= I2C_BIT_START;
-    iowrite32(auxReg, i2c_ptr + I2C_REG_CON);
+	// start condition
+    reg = ioread32(i2c_ptr + I2C_CON); 
+    reg |= I2C_CON_START;
+    iowrite32(reg, i2c_ptr + I2C_CON);
 
-    // Sends process to sleep
     wait_event_interruptible (waitq, waitq_cond != 0);
 
-    // We clear the start bit and sets the stop
-    auxReg = ioread32(i2c_ptr + I2C_REG_CON);
-    auxReg &= 0xFFFFFFFE;
-    auxReg |= I2C_BIT_STOP;
-    iowrite32(auxReg, i2c_ptr + I2C_REG_CON);
+    // stop condition
+    reg = ioread32(i2c_ptr + I2C_CON);
+    reg &= 0xFFFFFFFE;
+    reg |= I2C_CON_STOP;
+    iowrite32(reg, i2c_ptr + I2C_CON);
 
-    // Waits for the core to send the stop and frees the mutex.
     udelay(100); // 100 us
     mutex_unlock(&lock_bus);
     
 	if (waitq_cond > 0){
-        memcpy(read_buff, data_i2c.buff_rx, size);
+        memcpy(read_buff, hm_i2c.rx_buf, size);
         retval = 0;
     }
  
     return retval;
 }
 
-int i2c_read_reg(char slave_address, char reg_address, char* read_buff){
+/* --------------- FUNCIONES AUXILIARES ---------------*/
+
+static void i2c_set_slave(uint8_t addr){
+    iowrite32(addr, i2c_ptr + I2C_SA);
+}
+
+static void i2c_power_clock(void){
+
+    uint32_t aux;
+
+    aux = ioread32(clk_ptr + CM_PER_CLKCTRL); 
+    aux |= 0x02;
+	iowrite32(aux, clk_ptr + CM_PER_CLKCTRL);
+    
+	while(ioread32(clk_ptr + CM_PER_CLKCTRL) != CM_PER_CLKCTRL_ENA);
+}
+
+static int i2c_is_locked(void) {
+    
+	uint8_t i = 0;
+
+    while(mutex_is_locked(&lock_bus)) {
+    
+		msleep(1);
+        if (i++ == 100){
+            pr_err("BMP --> Timeout, el mutex esta bloqueado.\n");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+static void i2c_clear_bufs(void){
+
+    memset(hm_i2c.rx_buf, 0, 4096);
+    hm_i2c.rx_pos = 0;
+    hm_i2c.rx_len = 0;
+
+    memset(hm_i2c.tx_buf, 0, 4096);
+    hm_i2c.tx_pos = 0;
+    hm_i2c.tx_len = 0;
+}
+
+static int i2c_set_virq(struct platform_device *pdev){
 	
-    int retval = -1;
-    int auxReg;
-
-    // Wait until no other process is using it
-    if(i2c_is_locked() != 0) {
-        return retval;
+	if ((hm_i2c.virq = platform_get_irq(pdev, 0)) < 0) {
+        pr_err("BMP --> Error, no pudo obtenerse numero de IRQ.\n");
+        iounmap(i2c_ptr);
+		iounmap(clk_ptr);
+		iounmap(control_module_ptr);
+		return -1;
     }
 
-    // Get control of the bus
-    mutex_lock(&lock_bus);
-
-    // Makes sure CLK is running
-    i2c_power_clock();
-    
-    // Seteo la direccion del slave
-    i2c_set_slave(slave_address);
-    
-    // Load the data structures and registers.
-    i2c_clear_bufs();
-    data_i2c.buff_rx_len = 1;
-
-    // Load I2C DATA & CNT registers
-    iowrite32(reg_address, i2c_ptr + I2C_REG_DATA);
-    iowrite32(1, i2c_ptr + I2C_REG_CNT);
-
-    // Sets I2C CONFIG register w/ Master TX (=0x8600)
-    iowrite32(I2C_BIT_ENABLE | I2C_BIT_MASTER_MODE | I2C_BIT_TX, i2c_ptr + I2C_REG_CON);
-
-    // Clear IRQ Flags
-    iowrite32(I2C_IRQSTATUS_CLR_ALL, i2c_ptr + I2C_REG_IRQENABLE_CLR); 
-    iowrite32(I2C_IRQSTATUS_CLR_ALL, i2c_ptr + I2C_REG_IRQSTATUS);
-
-    // Enables ACK interrupt
-    iowrite32(I2C_IRQ_ARDY, i2c_ptr + I2C_REG_IRQENABLE_SET);
-
-    // Check irq status (occupied or free)
-    while (ioread32(i2c_ptr + I2C_REG_IRQSTATUS_RAW) & I2C_IRQ_BB) {msleep(1);}
-
-    // Sends START
-    waitq_cond = 0; 
-
-    auxReg = ioread32(i2c_ptr + I2C_REG_CON); 
-    auxReg |= I2C_BIT_START;
-    iowrite32(auxReg, i2c_ptr + I2C_REG_CON);
-
-    // Sends process to sleep
-    wait_event_interruptible (waitq, waitq_cond != 0);
-
-    // We clear the start bit and sets the stop
-    auxReg = ioread32(i2c_ptr + I2C_REG_CON);
-    auxReg &= 0xFFFFFFFE;
-    auxReg |= I2C_BIT_STOP;
-    iowrite32(auxReg, i2c_ptr + I2C_REG_CON);
-
-    // Waits for the core to send the stop and frees the mutex.
-    udelay(100); // 100 us
-    mutex_unlock(&lock_bus);
-    
-	if (waitq_cond > 0){
-        memcpy(read_buff, data_i2c.buff_rx, 1);
-        retval = 0;
+    if ((request_irq(hm_i2c.virq, (irq_handler_t) i2c_handler, IRQF_TRIGGER_RISING, "liam,i2c", NULL) < 0)){
+        pr_err("BMP --> Error, no pudo setearse la IRQ correctamente.\n");
+        iounmap(i2c_ptr);
+		iounmap(clk_ptr);
+		iounmap(control_module_ptr);
+		return -1;
     }
 
-    return retval;
+	return 0;
 }
